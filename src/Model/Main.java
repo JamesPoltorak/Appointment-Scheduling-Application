524c36531c4aed5f92ccd97791df92ee8172be7c
James Poltorak <jamesgpoltorak@hotmail.com>
2020-12-10 01:02:08 -0800

    Started project

---
+++ src/Model/Main.java
@@ -0,0 +1,38 @@
+package Model;
+
+import java.sql.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/View/sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+
+        try {
+            Connection myConn = DriverManager.getConnection("jdbc:mysql://wgudb.ucertify.com:3306/WJ05k6r", "U05k6r", "53688529325");
+            Statement myStmt = myConn.createStatement();
+            ResultSet myRs = myStmt.executeQuery("select * from countries");
+            while (myRs.next()) {
+                System.out.println(myRs.getString("Country"));
+            }
+        }
+        catch (Exception exc) {
+            exc.printStackTrace();
+        }
+
+        launch(args);
+
+    }
+}
