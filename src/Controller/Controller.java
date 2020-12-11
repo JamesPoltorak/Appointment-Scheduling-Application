25a6f02033d0bd2308f660a1dd2d9ccde8d8db8b
James Poltorak <jamesgpoltorak@hotmail.com>
2020-12-11 01:02:57 -0800

    Updated login screen

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,4 +1,32 @@
 package Controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.awt.event.ActionEvent;
 
 public class Controller {
+
+    @FXML
+    private TextField username_field;
+
+    @FXML
+    private TextField password_field;
+
+    @FXML
+    private Button login_button;
+
+    @FXML
+    private Button cancel_buttton;
+
+    @FXML
+    void cancel_button_action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void login_button_action(ActionEvent event) {
+
+    }
+
 }
